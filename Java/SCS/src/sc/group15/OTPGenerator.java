868d1e31d0f4268ff4b552b38c41af126ae177c9
epezhman <epezhman@gmail.com>
2014-12-01 22:16:34 +0100

    java fix

--- Java/SCS/src/sc/group15/OTPGenerator.java
+++ Java/SCS/src/sc/group15/OTPGenerator.java
@@ -1,11 +1,11 @@
 package sc.group15;
 
 import java.security.MessageDigest;
-import java.util.Random;
+//import java.util.Random;
 
 public class OTPGenerator {
 
-	private static int lastRand = -1;
+	//private static int lastRand = -1;
 	private static int cntr = 0;
 
 	public static String Generate(String PIN, String amount, String account) {
@@ -15,33 +15,33 @@ public class OTPGenerator {
 
 			MessageDigest md = MessageDigest.getInstance("SHA-256");
 
-			int interval = md.digest((toBeHashed + salt).getBytes("UTF-8"))[0];
-			interval = interval > 0 ? interval : -1 * interval;
-			interval = interval % 15;
-			boolean flag = true;
-			do {
-				interval = new Random().nextInt(interval);
-				if (interval != lastRand) {
-					lastRand = interval;
-					flag = false;
-				}
-
-			} while (flag);
+//			int interval = md.digest((toBeHashed + salt).getBytes("UTF-8"))[0];
+//			interval = interval > 0 ? interval : -1 * interval;
+//			interval = interval % 15;
+//			boolean flag = true;
+//			do {
+//				interval = new Random().nextInt(interval);
+//				if (interval != lastRand) {
+//					lastRand = interval;
+//					flag = false;
+//				}
+//
+//			} while (flag);
 
 			md.reset();
 			byte byteData[] = md.digest((toBeHashed + salt).getBytes("UTF-8"));
 
 			StringBuffer sb = new StringBuffer();
-			for (int i = 0; i < interval; i++) {
-				sb = new StringBuffer();
-				for (int j = 0; j < byteData.length; j++) {
-					sb.append(Integer
-							.toString((byteData[j] & 0xff) + 0x100, 16)
-							.substring(1));
-				}
-				md.reset();
-				byteData = md.digest(sb.toString().getBytes("UTF-8"));
-			}
+//			for (int i = 0; i < interval; i++) {
+//				sb = new StringBuffer();
+//				for (int j = 0; j < byteData.length; j++) {
+//					sb.append(Integer
+//							.toString((byteData[j] & 0xff) + 0x100, 16)
+//							.substring(1));
+//				}
+//				md.reset();
+//				byteData = md.digest(sb.toString().getBytes("UTF-8"));
+//			}
 
 			sb = new StringBuffer();
 			for (int i = 0; i < byteData.length; i++) {
